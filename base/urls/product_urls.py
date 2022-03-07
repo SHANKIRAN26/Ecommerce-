6049a910aa8ce71d6455e86f08cc4114af7e1f71
SHANKIRAN P M <shankiranpm26@gmail.com>
2022-03-07 19:49:25 +0530

    Add search functionality using query params

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -4,5 +4,10 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name="products"),
+    path('create/', views.createProduct, name='product_create'),
+    path('upload/', views.uploadImage, name='image_upload'),
+    path('<str:pk>/reviews/', views.createProductReview, name="create-review"),
     path('<str:pk>/', views.getProduct, name="product"),
+    path('delete/<str:pk>/', views.deleteProduct, name="delete_product"),
+    path('update/<str:pk>/', views.updateProduct, name="update_product"),
 ]
--- base/urls/user_urls.py
+++ base/urls/user_urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('profile/', views.getUserProfile, name="users_profile"),
     path('profile/update/', views.updateUserProfile, name="user_profile_update"),
     path('', views.getUsers, name="get_users"),
+
+    path('<int:pk>/', views.getUserById, name="user"),
+    path('update/<int:pk>/', views.updateUser, name="user_delete"),
+    path('delete/<int:pk>/', views.delete_user, name="user_update"),    
 ]
