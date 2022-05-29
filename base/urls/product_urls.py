7b90169a0b881cec87c74265826a981231c45015
SHANKIRAN P M <shankiranpm26@gmail.com>
2022-05-29 21:18:53 +0530

    Final commit

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -4,10 +4,14 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name="products"),
+
     path('create/', views.createProduct, name='product_create'),
     path('upload/', views.uploadImage, name='image_upload'),
+
     path('<str:pk>/reviews/', views.createProductReview, name="create-review"),
+    path('top/', views.getTopProducts, name="top-products"),
     path('<str:pk>/', views.getProduct, name="product"),
+
     path('delete/<str:pk>/', views.deleteProduct, name="delete_product"),
     path('update/<str:pk>/', views.updateProduct, name="update_product"),
 ]
